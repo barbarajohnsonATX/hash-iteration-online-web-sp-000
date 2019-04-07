92b14b9d9ce6721c1b88b4f53f421d8f6bf6fd3a
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-07 01:31:16 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,10 @@ def age_approparite_birthday(birthday_kids)
   
   birthday_kids.each do 
     |name, age|
-      
-      puts "Happy Birthday #{name}! You are now #{age} years old!" if age <= 12
+      if age <=12 
+        puts "Happy Birthday #{name}! You are now #{age} years old!" 
+      else 
+        puts "You are too old for this."
+      end 
     end 
 end  
